099a270bc97b0a8313afb16ce210f2e804a56fff
AdhurimBerisha51865 <ab51865@ubt-uni.net>
2025-05-31 19:15:38 +0200

    Database Connection

--- api/server.js
+++ api/server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import { connectMySQL } from "./config/mysql.js";
 
 // app config
 const app = express();
 const port = process.env.PORT || 4000;
+
+connectMySQL();
 
 // middlewares
 app.use(express.json());
