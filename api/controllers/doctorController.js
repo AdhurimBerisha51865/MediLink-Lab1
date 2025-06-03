88fedac35c319e333031fb954554c698754b63c0
AdhurimBerisha51865 <ab51865@ubt-uni.net>
2025-06-03 14:18:13 +0200

    Doctor Availability api function and route

---
+++ api/controllers/doctorController.js
@@ -0,0 +1,24 @@
+import { pool } from "../config/mysql.js";
+
+const changeAvailability = async (req, res) => {
+  try {
+    const { docId } = req.body;
+    const [[docData]] = await pool.execute(
+      `SELECT available FROM doctors WHERE id = ?`,
+      [docId]
+    );
+    if (!docData) {
+      return res.json({ success: false, message: "Doctor not found" });
+    }
+    await pool.execute(`UPDATE doctors SET available = ? WHERE id = ?`, [
+      !docData.available,
+      docId,
+    ]);
+    res.json({ success: true, message: "Availability Changed" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export { changeAvailability };
