a742eb61f7ae24ba91ee554d064c5dadc325745f
AdhurimBerisha51865 <ab51865@ubt-uni.net>
2025-06-03 14:23:31 +0200

    User registration and login

---
+++ api/controllers/userController.js
@@ -0,0 +1,62 @@
+import validator from "validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { pool } from "../config/mysql.js";
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      return res.json({ success: false, message: "Missing Details" });
+    }
+    if (!validator.isEmail(email)) {
+      return res.json({ success: false, message: "Enter a valid email" });
+    }
+    if (password.length < 8) {
+      return res.json({ success: false, message: "Enter a strong password" });
+    }
+    const [[existingUser]] = await pool.execute(
+      `SELECT * FROM users WHERE email = ?`,
+      [email]
+    );
+    if (existingUser) {
+      return res.json({ success: false, message: "User already exists" });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const [result] = await pool.execute(
+      `INSERT INTO users (name, email, password) VALUES (?, ?, ?)`,
+      [name, email, hashedPassword]
+    );
+    const userId = result.insertId;
+    const token = jwt.sign({ id: userId }, process.env.JWT_SECRET);
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const [[user]] = await pool.execute(`SELECT * FROM users WHERE email = ?`, [
+      email,
+    ]);
+    if (!user) {
+      return res.json({ success: false, message: "User does not exist" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) {
+      const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "Invalid Credentials" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export { registerUser, loginUser };
